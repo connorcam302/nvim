e9733b53f185ae2784ed47890426abecdcf81a1f
Connor <connorcam302@gmail.com>
2025-12-16 00:33:51 +0000

    added diagnostics

--- lua/config/scripts.lua
+++ lua/config/scripts.lua
@@ -1,6 +1,13 @@
-vim.api.nvim_create_autocmd('TextYankPost', {
-    group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
-    callback = function()
-        vim.highlight.on_yank()
-    end,
+vim.api.nvim_create_autocmd("TextYankPost", {
+	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
+
+vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+	group = vim.api.nvim_create_augroup("float_diagnostic", { clear = true }),
+	callback = function()
+		vim.diagnostic.open_float(nil, { focus = false, pos })
+	end,
 })
